bde230ec18b030c418572a93def2d162cb88977d
danah <danaalialq@icloud.com>
2022-10-27 15:14:29 +0300

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Aldanah Alqbbani on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
